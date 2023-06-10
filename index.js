cb938f2d2c8c708fd80b622d0bf01c058b575e8b
ayatbd <albehestione@gmail.com>
2023-06-10 21:10:15 +0600

    added users post

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ app.use(express.json());
 
 // ---------------------------
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y1sglpm.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,14 +23,31 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
+
+    const usersCollection = client.db("campDb").collection("users");
+
+    // users apis
+    app.post("/users", async(req, res) =>{
+        const user = req.body;
+        console.log(user);
+        const query = {email: user.email};
+        const existingUser = await usersCollection.findOne(query);
+        console.log(existingUser);
+        if (existingUser){
+            return res.send({ message: "the user is already existed"})
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    
+    // await client.close();
   }
 }
 run().catch(console.dir);
